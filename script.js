b5476e9cc86113eb04bfe1df2c62c44eff6335ca
Grant Register <register.grant@gmail.com>
2022-05-11 17:27:30 -0600

    Completely forgot to do this while I was working through it with my tutor

--- script.js
+++ script.js
@@ -1,19 +1,49 @@
-// Assignment code here
-function fun() {
-  prompt ("This is a prompt box", "Hello world");
-  }
+// Static variable
+const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const hasUppercaseLetters = uppercaseLetters.split("");
+const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+const hasLowercaseLetters = lowercaseLetters.split("");
+const numbers = "1234567890";
+const hasNumbers = numbers.split("");
+const symbols = "!@#$%^&*()[]{}:;',./<>?~`"
+const hasSymbols = symbols.split("");
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Write password to the #password input
+function writePassword() {
+    let allCharacters = [];
+    let createdPassword = "";
+
+    // Dynamic variables
+
+    var totalLengthPassword = prompt("How many characters long would you like your password to be?\n8-128");
+
+    if (totalLengthPassword < 8 || totalLengthPassword > 128) {
+      alert("Your password needs to be between 8 and 128 characters. Please try again.") 
+    } 
+    
+    // Confirming password parameters
+    else {
+      if (confirm("Do you want your password to contain uppercase letters?")) {
+        Array.prototype.push.apply(allCharacters, hasUppercaseLetters);
+      }
+     if (confirm("Do you want your password to contain lowercase letters?")) {
+        Array.prototype.push.apply(allCharacters, hasLowercaseLetters);
+      }
+     if (confirm("Do you want your effing password to contain numerals?")) {
+        Array.prototype.push.apply(allCharacters, hasNumbers);
+      } 
+    if (confirm("Do you want symbols in your password, ya punk?")) {
+        Array.prototype.push.apply(allCharacters, hasSymbols);
+    }
+    if (allCharacters.length === 0) {
+      alert("Not sure why this is so difficult for you to understand, but you gotta hit yes on one of those promts there, chief...");
+    }
+}
+
+}
